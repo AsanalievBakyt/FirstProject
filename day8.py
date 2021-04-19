6480c68f37f8b7858c487f41472e0eb9641c7481
AsanalievBakyt <asanali.bk@mail.ru>
2021-04-19 16:15:52 +0600

    create day7

---
+++ day8.py
@@ -0,0 +1,11 @@
+# import random
+#
+# random_numbers = random.sample(range(1, 30), 20)
+# print(random_numbers)
+#
+# for i in range(len(random_numbers)):
+#     print(i)
+
+string1 = 'hello'
+print(string1[0])
+
